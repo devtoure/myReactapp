958d616173e61077db927575b4c579cee4ba6e3e
Toure Abdoul razac <toureabdoulrazak9@gmail.com>
2020-09-05 05:32:29 +0000

    fix: target is not dom element

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 let element = React.createElement('h2', {} , 'Toure is React dev');
-console.log(element);
+
 let target = document.getElementById('#app');
-console.log(target);
-ReactDOM.render(element , target);
+ReactDOM.render(element , document.querySelector('#app'));
