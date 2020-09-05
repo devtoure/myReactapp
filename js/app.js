1d61820e69dddb3c25b75848635e691b3bb65bbc
Toure Abdoul razac <toureabdoulrazak9@gmail.com>
2020-09-05 05:20:22 +0000

    init projet

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let element = React.createElement('h2', {} , 'Toure is React dev');
+console.log(element);
+let target = document.getElementById('#app');
+console.log(target);
+ReactDOM.render(element , target);
